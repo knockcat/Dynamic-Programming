7f2a115436f01eb4a688aff77cbe7b0ba0da3dab
VICKY <vishujoshi21nov2000@gmail.com>
2022-01-29 14:11:08 +0530

    0-1 Knapsack Recursive

---
+++ 0-1 KNAPSACK Problems/0_1_knapsack.cpp
@@ -0,0 +1,31 @@
+// Recursive Implementation of 0-1 KnapSack Problem
+
+#include <iostream>
+using namespace std;
+
+int KnapSack(int wt[], int val[], int W, int n)
+{
+    // base Condition
+    if (n == 0 || W == 0)
+        return 0;
+    if (wt[n - 1] <= W)
+    {
+        //nth item included or not included
+        return max((val[n - 1] + KnapSack(wt, val, W - wt[n - 1], n - 1)), KnapSack(wt, val, W, n - 1));
+    }
+    else
+        return KnapSack(wt, val, W, n - 1);     //if wt is more item cannot be included
+}
+
+int main()
+{
+    int wt[] = {10 , 20  ,30 };
+    int val[] = {60, 100, 120};
+    int W = 50;
+
+    int n = sizeof(val) / sizeof(val[0]);
+
+    cout << KnapSack(wt, val, W, n);
+
+    return 0;
+}
